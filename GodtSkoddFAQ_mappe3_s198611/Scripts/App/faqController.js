7ca6eddc487c3dd67abb5c52d050730571bebba9
AnnikaH <annika_limfe@hotmail.com>
2015-11-12 23:00:04 +0100

    Update frontend

--- GodtSkoddFAQ_mappe3_s198611/Scripts/App/faqController.js
+++ GodtSkoddFAQ_mappe3_s198611/Scripts/App/faqController.js
@@ -6,7 +6,19 @@ App.controller("faqController", function ($scope, $http) {
     var urlRequest = '/api/Request';
     var urlCategory = '/api/Category';
     
-    // All ng-show: loading, faq, sendRequest, allRequests, newCategory, newFaq
+    /* All ng-show pages:
+        loading
+        faqPage
+        sendRequestPage
+        allFaqsPage
+        allRequestsPage
+        allCategoriesPage
+
+       All ng-show parts:
+        categoryPart (inni allCategoriesPage) (endre show inni her alt etter om har trykt "legg til ny" eller "endre")
+
+        faqPart (inni allFaqsPage) (endre show inni her alt etter om har trykt "legg til ny" eller "endre")
+    */
 
     // Code for what should happen/show on front page (FAQ):
     startPage();
@@ -16,8 +28,7 @@ App.controller("faqController", function ($scope, $http) {
         $scope.sendRequestPage = false;
         $scope.allFaqsPage = false;
         $scope.allRequestsPage = false;
-        $scope.newCategoryPage = false;
-        $scope.newFaqPage = false;
+        $scope.allCategoriesPage = false;
 
         $scope.loading = true;
         $scope.faqPage = true;
@@ -48,8 +59,7 @@ App.controller("faqController", function ($scope, $http) {
         $scope.faqPage = false;
         $scope.allFaqsPage = false;
         $scope.allRequestsPage = false;
-        $scope.newCategoryPage = false;
-        $scope.newFaqPage = false;
+        $scope.allCategoriesPage = false;
 
         $scope.loading = true;
         $scope.sendRequestPage = true;
@@ -60,8 +70,7 @@ App.controller("faqController", function ($scope, $http) {
         $scope.faqPage = false;
         $scope.sendRequestPage = false;
         $scope.allRequestsPage = false;
-        $scope.newCategoryPage = false;
-        $scope.newFaqPage = false;
+        $scope.allCategoriesPage = false;
 
         $scope.loading = true;
         $scope.allFaqsPage = true;
@@ -72,35 +81,64 @@ App.controller("faqController", function ($scope, $http) {
         $scope.faqPage = false;
         $scope.sendRequestPage = false;
         $scope.allFaqsPage = false;
-        $scope.newCategoryPage = false;
-        $scope.newFaqPage = false;
-
+        $scope.allCategoriesPage = false;
+        
         $scope.loading = true;
         $scope.allRequestsPage = true;
     }
 
+    // goToAllCategories()
+    $scope.goToAllCategories = function () {
+        $scope.faqPage = false;
+        $scope.sendRequestPage = false;
+        $scope.allRequestsPage = false;
+        $scope.allFaqsPage = false;
+        
+        $scope.loading = true;
+        $scope.allCategoriesPage = true;
+    }
+
+// ---------------------------- new and update ------------------------------
+
+    // ---------- Category ------------
+
     // goToNewCategory()
     $scope.goToNewCategory = function () {
-        $scope.faqPage = false;
+        /*$scope.faqPage = false;
         $scope.sendRequestPage = false;
         $scope.allFaqsPage = false;
         $scope.allRequestsPage = false;
-        $scope.newFaqPage = false;
 
         $scope.loading = true;
-        $scope.newCategoryPage = true;
+        $scope.allCategoriesPage = true;*/
+
+        $scope.categoryPart = true;
     }
+
+    // updateFAQ()
+    $scope.updateCategory = function () {
+        // i skjemaNyKategori
+    }
+
+    // registerFAQ()
+    $scope.registerCategory = function () {
+        // i skjemaNyKategori
+    }
+
+    // ------------- FAQ ---------------
 
     // goToNewFAQ()
     $scope.goToNewFAQ = function () {
-        $scope.faqPage = false;
+        /*$scope.faqPage = false;
         $scope.sendRequestPage = false;
         $scope.allFaqsPage = false;
         $scope.allRequestsPage = false;
         $scope.newCategoryPage = false;
 
         $scope.loading = true;
-        $scope.newFaqPage = true;
+        $scope.newFaqPage = true;*/
+
+        $scope.faqPart = true;
 
         // get all categories
         $http.get(urlCategory).
