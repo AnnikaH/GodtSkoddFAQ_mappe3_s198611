532ce8d4f429778dc20feb5ff104888154072324
AnnikaH <annika_limfe@hotmail.com>
2015-11-12 18:31:12 +0100

    Update frontend

--- GodtSkoddFAQ_mappe3_s198611/Scripts/App/faqController.js
+++ GodtSkoddFAQ_mappe3_s198611/Scripts/App/faqController.js
@@ -5,14 +5,74 @@ App.controller("faqController", function ($scope, $http) {
     var urlFAQ = '/api/FAQ';
     var urlRequest = '/api/Request';
     var urlCategory = '/api/Category';
+    
+    $scope.goToStartPage = function () {
+        window.location.reload;
+    }
 
-    $http.get(urlFAQ).
+    $scope.goToSendRequest = function () {
+        $scope.loading = false;
+        $scope.faq = false;
+        $scope.sendRequest = false;
+        $scope.allRequests = false;
+        $scope.newCategory = false;
+        $scope.newFaq = false;
+
+        $scope.$apply;
+    }
+
+    $scope.goToAllRequests = function () {
+        $scope.loading = false;
+        $scope.faq = false;
+        $scope.sendRequest = false;
+        $scope.allRequests = true;
+        $scope.newCategory = false;
+        $scope.newFaq = false;
+
+        $scope.$apply();
+    }
+
+    $scope.goToNewCategory = function () {
+        $scope.loading = false;
+        $scope.faq = false;
+        $scope.sendRequest = false;
+        $scope.allRequests = false;
+        $scope.newCategory = true;
+        $scope.newFaq = false;
+
+        $scope.$apply();
+    }
+
+    $scope.goToNewFAQ = function () {
+        $scope.loading = false;
+        $scope.faq = false;
+        $scope.sendRequest = false;
+        $scope.allRequests = false;
+        $scope.newCategory = false;
+        $scope.newFaq = true;
+
+        $scope.$apply();
+    }
+
+    // All ng-show: loading, faq, sendRequest, allRequests, newCategory, newFaq
+
+    // Code for what should happen/show on front page (FAQ):
+    $scope.loading = true;
+    $scope.faq = true;
+    $scope.sendRequest = true;
+    $scope.allRequests = true;
+    $scope.newCategory = true;
+    $scope.newFaq = true;
+
+    $scope.$apply();
+
+    /*$http.get(urlFAQ).
         success(function (allFAQs) {
             $scope.faq = true;
         }).
         error(function (data, status) {
             $scope.faq = false;
-        });
+        });*/
 
     /*
     var url = '/api/Kunde/';
